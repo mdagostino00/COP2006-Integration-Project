9cc9569674907c8bd505ce06799a8ad32726810a
mdagostino00 <80493791+mdagostino00@users.noreply.github.com>
2021-06-06 23:21:49 -0400

    Add files via upload

---
+++ Integration Projects/src/integrationPackage/Enemy.java
@@ -0,0 +1,18 @@
+package integrationPackage;
+// Michael D'Agostino
+
+import java.util.*;
+
+public class Enemy {
+	String name;
+	byte level;
+	double exp;
+	int strength;
+	int perception;
+	int endurance;
+	int charisma;
+	int intelligence;
+	int agility;
+	int luck;
+	int money;
+}
